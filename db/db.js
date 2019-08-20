195017cdcc2c5afe1a5d7eb66341b44faf525680
Andrew Meyers <ameyers.central@gmail.com>
2019-08-20 16:19:17 -0400

    first commit

---
+++ db/db.js
@@ -0,0 +1,19 @@
+const todos =  [
+    {
+      id: 1,
+      title: "lunch",
+      description: "Go for lunch by 2pm"
+    },
+    {
+      id: 2,
+      title: "coding",
+      description: "Code new Java program"
+    },
+    {
+      id: 3,
+      title: "celebrating",
+      description: "Created Test API"
+    }
+];
+
+export default todos;
